dca6a2586f8ddeef015cfdbd18a772de0f21821b
MNasybullin <mansurnasybullin@gmail.com>
2020-09-14 16:49:41 +0300

    add Project 12

---
+++ Project12b/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Stomach Diego on 9/4/20.
+//  Copyright © 2020 Mansur Nasybullin. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
